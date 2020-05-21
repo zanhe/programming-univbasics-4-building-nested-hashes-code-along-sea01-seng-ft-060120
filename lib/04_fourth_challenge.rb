6731f0b149139b7a46a801abdac873cd17b0d3bc
Zana H <zhertica@gmail.com>
2020-05-21 19:50:20 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,7 +5,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        }
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude:"hot-headed"
+        }
       ]
    }, 
    :capulet => {
@@ -13,7 +22,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          name: "",
+          age: "",
+          attitude: "",
+        }
       ]
    }
 }
