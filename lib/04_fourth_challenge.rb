99aaca94a61ea8539cf7f45503c789291bbb43ee
Zana H <zhertica@gmail.com>
2020-05-21 19:51:03 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -23,9 +23,14 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          name: "",
-          age: "",
-          attitude: "",
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        }
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
         }
       ]
    }
