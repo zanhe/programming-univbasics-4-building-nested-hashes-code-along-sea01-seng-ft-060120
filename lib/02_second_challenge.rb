6ff1ca192a56c35b7e91f230adb8175eff44d0d0
Zana H <zhertica@gmail.com>
2020-05-21 19:36:35 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,13 +5,13 @@ def second_challenge
       :matriarch => {}
       :hero => {}
       :hero_friends => []
-      }, 
+    }, 
     :capulet => {
       :patriarch => {}
       :matriarch => {}
       :heroine => {}
       :heroine_friends => []
-      }
+    }
   }
 
   
